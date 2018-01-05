719b14ecbbe96e36a2b671b5a78d6468e4db1a9c
WilbertAlfonso <wilbert.marroquin@ucsp.edu.pe>
2018-01-05 14:02:00 -0500

    Agregando SMS Controller (Formato de Envio)

--- Controllers/SMSController.cs
+++ Controllers/SMSController.cs
@@ -16,6 +16,8 @@ namespace sms_project.Controllers
         public IActionResult Index()
         {
             // Your Account SID from twilio.com/console
+
+            //Agreguen su Credenciales de Twilion
             var accountSid = "AC9f96b28edbb32a3c006942d55e075e62";
             // Your Auth Token from twilio.com/console
             var authToken = "3e29e0079c3303d6b7d67e364b56700e";   
@@ -28,8 +30,6 @@ namespace sms_project.Controllers
                 body: "Hello from C#");
 
             Console.WriteLine(message.Sid);
-            Console.Write("Press any key to continue.");
-            Console.ReadKey();
             ViewData["Message"] = "Mensaje Enviado";
             return View();
         }
