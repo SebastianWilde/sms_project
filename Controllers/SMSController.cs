304336f7a6c41d51d0864513cd76c5bd790747f2
WilbertAlfonso <wilbert.marroquin@ucsp.edu.pe>
2018-01-08 11:11:11 -0500

    Prueba de Autorizacion

--- Controllers/SMSController.cs
+++ Controllers/SMSController.cs
@@ -31,25 +31,35 @@ namespace sms_project.Controllers
             // Your Account SID from twilio.com/console
 
             //Agreguen su Credenciales de Twilion
-            var accountSid = "AC9f96b28edbb32a3c006942d55e075e62";
+            var accountSid = "ACe015558675ea7b8edda3f40da3eb28e4";
             // Your Auth Token from twilio.com/console
-            var authToken = "3e29e0079c3303d6b7d67e364b56700e";   
+            var authToken = "3ce3d327d64356a8a1ff5de89f5ebf43";   
 
             TwilioClient.Init(accountSid, authToken);
 
             
-            var people = getNumeros();
+            var people = new Dictionary<string, string>() {
+                {"+51959741515", "Sebas"},
+                {"+51972892866", "Wilbur"}
+            };
+            /*var phoneNumber = new PhoneNumber("+51959741515");
+            var validationRequest = ValidationRequestResource.Create(
+                phoneNumber,
+                friendlyName: "Sebas Number");
+            
+            Console.WriteLine(validationRequest.ValidationCode);
+            */
             // Iterate over all our friends
             foreach (var person in people)
             {
                 // Send a new outgoing SMS by POSTing to the Messages resource
-                /*MessageResource.Create(
-                    from: new PhoneNumber("+15013024007"), // From number, must be an SMS-enabled Twilio number
+                MessageResource.Create(
+                    from: new PhoneNumber("+15042245074"), // From number, must be an SMS-enabled Twilio number
                     to: new PhoneNumber(person.Key), // To number, if using Sandbox see note above
                                                      // Message content
                     body: $"Hey {person.Value} Monkey Party at 6PM. Bring Bananas!");
-                */
-                Console.WriteLine("Enviando a Numero: {0}",person);
+
+                Console.WriteLine($"Sent message to {person.Value}");
             }
             ViewData["Message"] = "Mensaje Enviado";
             return View();
