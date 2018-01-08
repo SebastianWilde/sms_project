54e4e81c431da2684da8c7ee0ca07289cd451863
WilbertAlfonso <wilbert.marroquin@ucsp.edu.pe>
2018-01-07 22:51:44 -0500

    Prueba de Multiples Mensajes

--- Controllers/SMSController.cs
+++ Controllers/SMSController.cs
@@ -13,6 +13,19 @@ namespace sms_project.Controllers
 {
     public class SMSController : Controller
     {
+        
+        private readonly sms_projectContext _context;
+        public SMSController(sms_projectContext context)
+        {
+            _context = context;
+        }
+        public List<string> getNumeros()
+        {
+            var test = from s in _context.Movie
+                       orderby s.numero
+                       select s.numero;
+            return test.ToList();
+        }
         public IActionResult Index()
         {
             // Your Account SID from twilio.com/console
@@ -24,12 +37,20 @@ namespace sms_project.Controllers
 
             TwilioClient.Init(accountSid, authToken);
 
-            var message = MessageResource.Create(
-                to: new PhoneNumber("+51972892866"),
-                from: new PhoneNumber("+15013024007"),
-                body: "Hello from C#");
-
-            Console.WriteLine(message.Sid);
+            
+            var people = getNumeros();
+            // Iterate over all our friends
+            foreach (var person in people)
+            {
+                // Send a new outgoing SMS by POSTing to the Messages resource
+                /*MessageResource.Create(
+                    from: new PhoneNumber("+15013024007"), // From number, must be an SMS-enabled Twilio number
+                    to: new PhoneNumber(person.Key), // To number, if using Sandbox see note above
+                                                     // Message content
+                    body: $"Hey {person.Value} Monkey Party at 6PM. Bring Bananas!");
+                */
+                Console.WriteLine("Enviando a Numero: {0}",person);
+            }
             ViewData["Message"] = "Mensaje Enviado";
             return View();
         }
