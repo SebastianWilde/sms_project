0476c41e6d6a71ebdb2a54284fa91b67680caa2e
Sebastian Wilde Alarcon Arenas <sebastian.alarcon@ucsp.edu.pe>
2018-01-12 10:59:34 -0500

    db

--- Startup.cs
+++ Startup.cs
@@ -23,8 +23,16 @@ namespace sms_project
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<sms_projectContext>(options =>
-            options.UseSqlite("Data Source=sms_project.db"));
+            // Use SQL Database if in Azure, otherwise, use SQLLite
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                services.AddDbContext<sms_projectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            else
+                services.AddDbContext<sms_projectContext>(options =>
+                    options.UseSqlite("Data Source=sms_project.db"));
+
+            // Automatically perform database migration
+            services.BuildServiceProvider().GetService<sms_projectContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
