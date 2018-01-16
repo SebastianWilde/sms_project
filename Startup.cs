fdb1930d65f8b21e1cb36edf437ed954b2bf68d3
Sebastian Wilde Alarcon Arenas <sebastian.alarcon@ucsp.edu.pe>
2018-01-16 10:59:52 -0500

    intento 12

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace sms_project
             // Use SQL Database if in Azure, otherwise, use SQLLite
             if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
                 services.AddDbContext<sms_projectContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+                    options.UseSqlServer(Configuration.GetConnectionString("sms_projectContext")));
             else
                 services.AddDbContext<sms_projectContext>(options =>
                     options.UseSqlite("Data Source=sms_project.db"));
